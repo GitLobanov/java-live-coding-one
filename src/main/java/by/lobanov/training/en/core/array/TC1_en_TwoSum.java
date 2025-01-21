9f072c4c96a59329e6ec0a0848f03b5ecc035df8
GitLobanov <lobanovgithub@gmail.com>
2025-01-21 05:14:39 +0300

    New AddTwoNumbers, edit TwoSum

--- src/main/java/by/lobanov/training/en/core/array/TC1_en_TwoSum.java
+++ src/main/java/by/lobanov/training/en/core/array/TC1_en_TwoSum.java
@@ -22,12 +22,18 @@ public class TC1_en_TwoSum {
 
     public static void main(String[] args) {
         int [] arr = Arrays.stream("2,7,11,15".split(",")).mapToInt(Integer::parseInt).toArray();
-        System.out.println(twoSum(arr, 9));
+        System.out.println(Arrays.toString(twoSum(arr, 9)));
     }
 
     public static int[] twoSum(int[] nums, int target) {
-//        for ()
-        return null;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (j == i) continue;
+                if ((nums[i] + nums[j]) == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[2];
     }
-
 }
